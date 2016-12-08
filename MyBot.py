e78bf2c163ee9090d38322324824128b8ec59f2e
Max Kingsbury <max.kingsbury@gmail.com>
2016-12-07 17:20:11 -0800

    logic for in friendly territory vs on the border
    Armies check to see if they are on the border, if not, just shuffle
    diagonally, if so, wait until a capture is available

--- MyBot.py
+++ MyBot.py
@@ -4,6 +4,13 @@ from networking import *
 myID, gameMap = getInit()
 sendInit("MyPythonBot")
 
+def isInFriendlyTerritory(location):
+    retVal = True
+    for dir in CARDINALS:
+        if(gameMap.getSite(location, dir).owner != myID):
+            retVal = False
+    return retVal
+
 while True:
     moves = []
     gameMap = getFrame()
@@ -11,19 +18,21 @@ while True:
         for x in range(gameMap.width):
             location = Location(x, y)
             if gameMap.getSite(location).owner == myID:
-                # if not full strength, move if you can capture a site
                 mystrength = gameMap.getSite(location).strength
-                capturefound = False
-                for dir in CARDINALS:
-                    if ((gameMap.getSite(location, dir).owner != myID) and (mystrength >= gameMap.getSite(location, dir).strength)):
-                        moves.append(Move(location, dir))
-                        capturefound = True
-                        break
-                # if full strength, move either north or west, depending on your location
-                if((capturefound == False) and (mystrength > (6 * gameMap.getSite(location).production))):
-                    if (x+y)%2 == 1:
-                        moves.append(Move(location, NORTH))
-                    else:
-                        moves.append(Move(location, WEST))
+                # if at high strength, and in friendly territory, move either north or west, depending on your location
+                if (isInFriendlyTerritory(location)):
+                    if(mystrength > (6 * gameMap.getSite(location).production)):
+                        if (x+y)%2 == 1:
+                            moves.append(Move(location, NORTH))
+                        else:
+                            moves.append(Move(location, WEST))
+                # if on the border, move if you can capture a site
+                else:
+                    capturefound = False
+                    for dir in CARDINALS:
+                        if ((gameMap.getSite(location, dir).owner != myID) and (mystrength >= gameMap.getSite(location, dir).strength)):
+                            moves.append(Move(location, dir))
+                            capturefound = True
+                            break
 
     sendFrame(moves)
